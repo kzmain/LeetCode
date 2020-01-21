13a6016e481251fa8377145b9e9a87ec5e307e5f
Kai Zhang <main@kai.sh>
2020-01-21 18:20:36 +0100

    0859｜EASY｜Buddy Strings（Bug fix）
    Fix 前一个版本的Bug前一个版本只要前后有一样的char就会返回为true，这是不对的，虽然能pass leetcode的测试但是不对

--- 0859｜EASY｜Buddy Strings/method1.cpp
+++ 0859｜EASY｜Buddy Strings/method1.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <set>
 
 
@@ -7,23 +6,19 @@ class Solution {
 public:
     bool buddyStrings(std::string A, std::string B) {
         if(A.size() != B.size()) return false;
+        if(A == B){
+            std::sort(A.begin(), A.end());
+            for(int i = 1; i < A.size(); i++) if(A[i] == A[i - 1])return true;
+            return false;
+        }
         int pos1 = -1, pos2 = -1;
         for(int i = 0; i < A.size(); i++){
             //Try to find at least two different chars
-            if(A[i] != B[i]) if(pos1==-1){pos1 = i;}else{pos2 = i; break;}
-            //Two continues chars are the same
-            else if(i + 1 < A.size() and A[i] == A[i + 1]){return true;}
-        }
-        //Two string are totally the same
-        if(pos1 == -1){
-            std::sort(A.begin(), A.end());
-            for(int i = 1; i < A.size(); i++) if(A[i - 1] == A[i]) return true;
-            return false;
+            if(A[i] != B[i]){if(pos1==-1){pos1 = i;}else{pos2 = i; break;}}
         }
         //A and B only have one different char
-        else if(pos2 == -1){
-            return false;
-        }
+        if(pos1 == -1)     return false;
+        else if(pos2 == -1)return false;
         //A and B have two different chars
         else{
             A[pos1] += A[pos2];
